baf9302fde8adbf581697cb1c8648ed081e21885
Eliana Perez Ramos <eliana.perez@quipux.com>
2025-03-25 11:56:02 -0500

    Se empezo el 4 escenario en la documentación

---
+++ src/test/java/com/quipux/SemilleroAutomatizacion/stepdefinitions/CarritoDeComprasStepDefinitions.java
@@ -0,0 +1,21 @@
+package com.quipux.SemilleroAutomatizacion.stepdefinitions;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class CarritoDeComprasStepDefinitions {
+    @Dado("que me encuentro en la informacion del primero articulo")
+    public void queMeEncuentroEnLaInformacionDelPrimeroArticulo() {
+
+    }
+    @Cuando("le doy click al boton de agregar al carrito")
+    public void leDoyClickAlBotonDeAgregarAlCarrito() {
+
+
+    }
+    @Entonces("El articulo debe de estar en el carrito")
+    public void elArticuloDebeDeEstarEnElCarrito() {
+
+    }
+}
--- src/test/resources/features/carrito_de_compras.feature
+++ src/test/resources/features/carrito_de_compras.feature
@@ -1,1 +1,8 @@
 #language:es
+  @AgregarArticulo
+  Característica: Realizo pruebas en el carrito de compras
+
+    Escenario: Agrego un articulo al carrito de compras
+      Dado que me encuentro en la informacion del primero articulo
+      Cuando le doy click al boton de agregar al carrito
+      Entonces  El articulo debe de estar en el carrito
